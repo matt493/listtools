a3fd9d14545c007304a07690b81e16cd6d8535d8
mathew_reji <mathewreji493@gmail.com>
2023-01-28 15:07:31 +0530

    dummy implementations

--- extension.js
+++ extension.js
@@ -1,34 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "listtools" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('listtools.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ListTools!');
+	let remDupelines = vscode.commands.registerCommand('listtools.removeDuplicateLines', function () {
+		vscode.window.showInformationMessage('Removing duplicate lines');
+		let op = vscode.commands.executeCommand("editor.action.removeDuplicateLines");
+		console.log(op.then())
 	});
+	context.subscriptions.push(remDupelines);
 
-	context.subscriptions.push(disposable);
+	let sortLinesAsc = vscode.commands.registerCommand('listtools.sortLinesAscending', function () {
+		vscode.window.showInformationMessage('Sorting lines ascending',);
+		vscode.commands.executeCommand("editor.action.sortLinesAscending");
+	});
+	context.subscriptions.push(sortLinesAsc);
+
+	let sortLinesDesc = vscode.commands.registerCommand('listtools.sortLinesDescending', function () {
+		vscode.window.showInformationMessage('Sorting lines descending',);
+		vscode.commands.executeCommand("editor.action.sortLinesDescending");
+	});
+	context.subscriptions.push(sortLinesDesc);
+
+	let findDupeLines = vscode.commands.registerCommand('listtools.findDuplicateLines', function () {
+		vscode.window.showInformationMessage('Finding duplicate lines',);
+		//TODO: Find duplicate lines
+	});
+	context.subscriptions.push(findDupeLines);
+
+	let findUniqueLines = vscode.commands.registerCommand('listtools.findUniqueLines', function () {
+		vscode.window.showInformationMessage('Finding unique lines',);
+		//TODO: Find unique lines
+	});
+	context.subscriptions.push(findUniqueLines);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
